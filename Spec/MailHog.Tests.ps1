c9b4652e3422e4a2ca7e0e6ac347400b537bbec7
Bart Dubois <dubcio@o2.pl>
2018-12-04 23:04:34 +0100

    Specify image acceptance criteria

---
+++ Spec/MailHog.Tests.ps1
@@ -0,0 +1,41 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingCmdletAliases", "")]
+
+param (
+    [Parameter(Mandatory = $true)]
+    $ImageName,
+
+    $Here = (Split-Path -Parent $MyInvocation.MyCommand.Path),
+    $ModulesPath = (Join-Path $Here "Modules")
+)
+
+Import-Module (Join-Path $ModulesPath 'TestDrive.psm1') -Force
+
+
+Describe "MailHog image" {
+
+    It "starts successfuly" {
+        Write-Error "FIXME: implement case"
+    }
+
+    It "accepts emails" {
+        Write-Error "FIXME: implement case"
+    }
+
+    Context "Web UI" {
+
+        It "is accessable" {
+            Write-Error "FIXME: implement case"
+        }
+    }
+
+    Context "Web API" {
+
+        It "exposess accepted emails" {
+            Write-Error "FIXME: implement case"
+        }
+
+        It "allows to remove accepted emails" {
+            Write-Error "FIXME: implement case"
+        }
+    }
+}
